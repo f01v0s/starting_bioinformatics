d5282f991d71cc5f6920a81c0a1e6cd4f18f5bdc
ZisisF <131684601+f01v0s@users.noreply.github.com>
2024-09-25 15:48:34 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print((5 + 3) - 6**2 / 3)
+# Calculate the remaining hours
+hours = 856 // 60
+print(hours)
+
+# Calculate the remaining minutes
+remaining_minutes = 856 % 60
+print(remaining_minutes)
